b1ab3ef344716205ecbb61eb21db6d9bbe1a94ca
Khaled Ahmed <khaled.ahmed@elwatannews.com>
2025-03-03 10:28:23 +0200

    client side router param guard

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -31,6 +31,18 @@ const router = createBrowserRouter([
       {
         path: "products:prefix",
         element: <Products />,
+        loader: ({ params }) => {
+          if (
+            typeof params.prefix !== "string" ||
+            !/^[a-z]+$/i.test(params.prefix)
+          ) {
+            throw new Response("Bad Request", {
+              statusText: "Category Not Found",
+              status: 400,
+            });
+          }
+          return true;
+        },
       },
       {
         path: "about-us",
