8a556dc483716434b27e71568cf9cbedb36e978f
unknown <khaled.ahmed@elwatannews.com>
2026-03-29 21:18:47 +0200

    promise

--- Ecommerce-Front/src/main.tsx
+++ Ecommerce-Front/src/main.tsx
@@ -1,7 +1,16 @@
 import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "./routes/AppRouter";
+
+// Styles
 import "@styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// Redux
+import { Provider } from "react-redux";
+import { store } from "@store/store";
 
-createRoot(document.getElementById("root")!).render(<AppRouter />);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+);
